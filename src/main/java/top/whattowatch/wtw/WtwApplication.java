ad28e981ada6ad0a3ae0b656338a17e8c1e4e370
xyj1209 <15817061252@163.com>
2018-07-25 20:35:51 +0800

    添加应用监控注解

--- src/main/java/top/whattowatch/wtw/WtwApplication.java
+++ src/main/java/top/whattowatch/wtw/WtwApplication.java
@@ -1,5 +1,6 @@
 package top.whattowatch.wtw;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @MapperScan("top.whattowatch.wtw.mapper")
 @ServletComponentScan
+@EnableAdminServer
 public class WtwApplication {
 
     public static void main(String[] args) {
