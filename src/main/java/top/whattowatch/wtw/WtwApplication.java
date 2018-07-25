f17492a2281db09827f9c2af990b44ed44473011
xyj1209 <15817061252@163.com>
2018-07-25 19:30:41 +0800

    修改启动类，添加mapper扫描，druid配置类扫描

--- src/main/java/top/whattowatch/wtw/WtwApplication.java
+++ src/main/java/top/whattowatch/wtw/WtwApplication.java
@@ -1,9 +1,13 @@
 package top.whattowatch.wtw;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@MapperScan("top.whattowatch.wtw.mapper")
+@ServletComponentScan
 public class WtwApplication {
 
     public static void main(String[] args) {
