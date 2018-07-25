fa60bdddc1956fdcfa52353bbe04e9329da889a1
xyj1209 <15817061252@163.com>
2018-07-25 10:46:18 +0800

    Initial commit

---
+++ src/main/java/top/whattowatch/wtw/WtwApplication.java
@@ -0,0 +1,12 @@
+package top.whattowatch.wtw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtwApplication.class, args);
+    }
+}
