045a3d7901505db9a10621f27d78afae49520335
xyj1209 <15817061252@163.com>
2018-07-26 15:07:22 +0800

    添加启动任务调度功能的注解

--- src/main/java/top/whattowatch/wtw/WtwApplication.java
+++ src/main/java/top/whattowatch/wtw/WtwApplication.java
@@ -5,11 +5,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @MapperScan("top.whattowatch.wtw.mapper")
 @ServletComponentScan
 @EnableAdminServer
+@EnableScheduling
 public class WtwApplication {
 
     public static void main(String[] args) {
