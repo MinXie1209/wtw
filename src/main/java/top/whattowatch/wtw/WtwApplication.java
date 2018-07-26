a23c7726c6a207a68d314a90c1066f7121a13a41
xyj1209 <15817061252@163.com>
2018-07-26 16:11:09 +0800

    updata app

--- src/main/java/top/whattowatch/wtw/WtwApplication.java
+++ src/main/java/top/whattowatch/wtw/WtwApplication.java
@@ -1,12 +1,12 @@
 package top.whattowatch.wtw;
 
+
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @MapperScan("top.whattowatch.wtw.mapper")
