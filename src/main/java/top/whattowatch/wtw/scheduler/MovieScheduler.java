353cbc1f0718f46bb317d1a6b5352c8982fca032
xyj1209 <15817061252@163.com>
2018-07-26 15:05:27 +0800

    添加定时爬取数据类

---
+++ src/main/java/top/whattowatch/wtw/scheduler/MovieScheduler.java
@@ -0,0 +1,31 @@
+package top.whattowatch.wtw.scheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import top.whattowatch.wtw.mapper.MovieMapper;
+import top.whattowatch.wtw.po.Movie;
+import top.whattowatch.wtw.webmagic.MoviePageProcessor;
+
+import java.util.ArrayList;
+
+/**
+ * @author 江南小俊
+ * @create 2018-07-26 12:47
+ * @desc 任务调度定时爬取电影资源
+ **/
+@Component
+public class MovieScheduler {
+    @Autowired
+    MovieMapper movieMapper;
+
+    @Scheduled(cron = "10 10 15 26/7 * ?")
+    @Transactional(rollbackFor = Exception.class)
+    public void start() {
+        MoviePageProcessor moviePageProcessor = new MoviePageProcessor();
+        ArrayList<Movie> movies = moviePageProcessor.main();
+        movieMapper.deleteByExample(null);
+        movieMapper.insertBatch(movies);
+    }
+}
